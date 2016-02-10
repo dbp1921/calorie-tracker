ee046fbe6480ade4aac775c4dd9c1f2600b61ea5
Jussi Lundstedt <jussi@wharton.upenn.edu>
2016-02-10 14:28:09 -0500

    created activities

---
+++ CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/SettingsActivity.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis350.group1.calorietracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
