7cbbcaaf8a6441139c6959aff006f299feb3faba
jibreel <jibreel.powell@gmail.com>
2016-02-22 03:22:59 -0500

    Finished for Iterations 1: Custom Calendar working, with date highlighting for current date, current month and selected. Need to coordinate with Jussi on SQL

--- CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/CustomCalendarView.java
+++ CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/CustomCalendarView.java
@@ -2,23 +2,22 @@ package edu.upenn.cis350.group1.calorietracker;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.zip.Inflater;
 
 /**
  * Created by jibreel on 2/21/16.
@@ -53,17 +52,26 @@ public class CustomCalendarView extends LinearLayout {
     // Format for dates
     static final String DATE_FORMAT = "MMMM yyyy";
 
+    //Date Change Listener
+    private OnDateChangeListener listener;
+
+
+    interface OnDateChangeListener {
+
+        public abstract void onSelectedDayChange(CustomCalendarView view, int year, int month,
+                                                  int dayOfMonth);
+    }
 
     private class GridAdapter extends BaseAdapter {
 
-        Calendar[] dates;
+        ArrayList<Calendar> dates;
         Context context;
         DatabaseHandler dbHandler;
         LayoutInflater inflater;
 
         //Constructor for gridAdapter
         public GridAdapter(Context context, ArrayList<Calendar> dates, DatabaseHandler dbHandler){
-            this.dates = (Calendar[]) dates.toArray();
+            this.dates = dates;
             this.context = context;
             this.dbHandler = dbHandler;
             this.inflater = LayoutInflater.from(context);
@@ -71,12 +79,12 @@ public class CustomCalendarView extends LinearLayout {
 
         @Override
         public int getCount() {
-            return dates.length;
+            return dates.size();
         }
 
         @Override
         public Object getItem(int position) {
-            return dates[position];
+            return dates.get(position);
         }
 
         @Override
@@ -86,13 +94,36 @@ public class CustomCalendarView extends LinearLayout {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            Calendar date = dates[position];
+            Calendar date = dates.get(position);
             TextView view = (convertView != null) ? (TextView) convertView :
-                    (TextView) inflater.inflate(R.layout.date_view, parent);
-            view.setText(String.valueOf(date.DAY_OF_MONTH));
+                    (TextView) inflater.inflate(R.layout.date_view, parent, false);
+            view.setText(String.valueOf(date.getTime().getDate()));
 
-            if (dbHandler.getDateID(new java.sql.Date(date.getTimeInMillis())) != -1) {
-                view.setTextColor(Color.MAGENTA);
+            view.setTag(R.id.date_key, date.clone());
+            Log.v("tag set as ", date.getTime().toString());
+
+            //TODO change date color based on database information
+//            if (dbHandler.getDateID(new java.sql.Date(date.getTimeInMillis())) != -1) {
+//                view.setTextColor(Color.CYAN);
+//            }
+
+
+            if(date.getTime().getYear() == Calendar.getInstance().getTime().getYear()) {
+                if (date.getTime().getMonth() == Calendar.getInstance().getTime().getMonth()) {
+                    view.setTextColor(Color.BLACK);
+
+                    if (date.getTime().getDate() == Calendar.getInstance().getTime().getDate()) {
+                        view.setTextColor(Color.MAGENTA);
+                    }
+                }
+            }
+
+            if(date.getTime().getYear() == dateHolder.getTime().getYear()) {
+                if (date.getTime().getMonth() == dateHolder.getTime().getMonth()) {
+                    if (date.getTime().getDate() == dateHolder.getTime().getDate()) {
+                        view.setTypeface(null, Typeface.BOLD);
+                    }
+                }
             }
 
             return view;
@@ -100,7 +131,7 @@ public class CustomCalendarView extends LinearLayout {
     }
 
     /**
-     * Single argument contructor takes in only Context
+     * Single argument constructor takes in only Context
      * @param context
      */
     public CustomCalendarView(Context context) {
@@ -130,7 +161,7 @@ public class CustomCalendarView extends LinearLayout {
     private void inflateCalendar(Context context) {
         LayoutInflater inflater = LayoutInflater.from(context);
 
-        inflater.inflate(R.layout.calendar_layout, this);
+        inflater.inflate(R.layout.custom_calendar_view, this);
 
         calendarMonth = (TextView) findViewById(R.id.curr_month);
         weekDays =(LinearLayout) findViewById(R.id.days_of_the_week);
@@ -149,7 +180,7 @@ public class CustomCalendarView extends LinearLayout {
             }
         });
 
-        prevButton.setOnClickListener(new OnClickListener() {
+        nextButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 dateHolder.add(Calendar.MONTH, 1);
@@ -169,31 +200,61 @@ public class CustomCalendarView extends LinearLayout {
 
         Calendar calendar = (Calendar) dateHolder.clone();
 
+        if (listener != null) {
+            listener.onSelectedDayChange(this, calendar.YEAR, calendar.MONTH,
+                    calendar.DAY_OF_MONTH);
+        }
+
         //Find first day of month
         calendar.set(Calendar.DAY_OF_MONTH, 1);
 
+
         //What day of the week is the first of the month
         int firstVisibleDate = calendar.get(Calendar.DAY_OF_WEEK) - 1;
+
 
         //Display current month at top of view
         calendarMonth.setText(new SimpleDateFormat(DATE_FORMAT).format(calendar.getTime()));
 
         //Add the days of the last month that will bring us to a full week
-        calendar.add(Calendar.DAY_OF_MONTH, 0 - firstVisibleDate);
+        calendar.add(Calendar.DAY_OF_YEAR, -firstVisibleDate);
+
 
         //Add the rest of the days to the list, totalling 6 weeks
         while(dates.size() < NUM_DAYS) {
             dates.add((Calendar) calendar.clone());
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
+            calendar.add(Calendar.DAY_OF_YEAR, 1);
         }
 
         calendarGrid.setAdapter(new GridAdapter(getContext(), dates,
                 new DatabaseHandler(context.getApplicationContext())));
 
+
+        calendarGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView) view;
+                //textView.setTypeface(null, Typeface.BOLD);
+                Calendar selectedDate = (Calendar) textView.getTag(R.id.date_key);
+                dateHolder = selectedDate;
+                updateCalendar();
+            }
+        });
     }
 
+    /**
+     * Returns the current date (in milliseconds) as a long
+     * @return
+     */
     public long getDate() {
         return dateHolder.getTimeInMillis();
     }
+
+    public void setOnDateChangeListener(OnDateChangeListener listener) {
+        this.listener = listener;
+    }
+
+
+
 
 }
