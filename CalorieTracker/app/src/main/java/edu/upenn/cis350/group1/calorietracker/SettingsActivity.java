3bdac08e75787c180ed38041e1c8433fd9b3cdbe
Fedrick Finjap <fefi@wharton.upenn.edu>
2016-03-20 15:39:19 -0400

    Added setting for user daily preferred caloric intake, TODO: save the state of the preference after activity is destroyed

--- CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/SettingsActivity.java
+++ CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/SettingsActivity.java
@@ -2,12 +2,45 @@ package edu.upenn.cis350.group1.calorietracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    public static final String calorieKey = "Calorie Limit";
+    public static String caloricLimit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        if (savedInstanceState != null) {
+            EditText setting = (EditText) findViewById(R.id.calorie_limit);
+            caloricLimit = savedInstanceState.getString(calorieKey);
+            setting.setText(caloricLimit);
+        } else {
+            EditText setting = (EditText) findViewById(R.id.calorie_limit);
+            caloricLimit = "2000";
+            setting.setText(caloricLimit);
+        }
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString(calorieKey, caloricLimit);
+
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    public void saveSettings(View v) {
+        setContentView(R.layout.activity_settings);
+        EditText setting = (EditText) findViewById(R.id.calorie_limit);
+        caloricLimit = setting.getText().toString();
+        setting.setText(caloricLimit);
+
+        finish();
+
+    }
+
 }
