1b2944f1887f7a9340b018e80df7643758f2e1d3
jibreel <jibreel.powell@gmail.com>
2016-03-20 23:42:50 -0400

    Date green if under limit, red if over

--- CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/SettingsActivity.java
+++ CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/SettingsActivity.java
@@ -2,6 +2,7 @@ package edu.upenn.cis350.group1.calorietracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
