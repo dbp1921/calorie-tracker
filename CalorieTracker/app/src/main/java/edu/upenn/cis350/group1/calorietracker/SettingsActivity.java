e56148a4585b39977d3ea23da11b0b3dc92983af
Fedrick Finjap <fefi@wharton.upenn.edu>
2016-04-03 23:34:05 -0400

    Updated settings with values for protein, sodium, fat, and carb intake

--- CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/SettingsActivity.java
+++ CalorieTracker/app/src/main/java/edu/upenn/cis350/group1/calorietracker/SettingsActivity.java
@@ -12,6 +12,14 @@ public class SettingsActivity extends CalorieTrackerActivity {
 
     public static final String calorieKey = "calories";
     public static int caloricLimit;
+    public static final String proteinKey = "protein";
+    public static int proteinLimit;
+    public static final String carbKey = "carbs";
+    public static int carbLimit;
+    public static final String fatKey = "fat";
+    public static int fatLimit;
+    public static final String sodiumKey = "sodium";
+    public static int sodiumLimit;
     private DatabaseHandler db;
     //test changes
 
@@ -28,6 +36,38 @@ public class SettingsActivity extends CalorieTrackerActivity {
             db.addSetting(calorieKey, caloricLimit);
         }
         setting.setText(Integer.toString(caloricLimit));
+
+        EditText settingProt = (EditText) findViewById(R.id.protein_limit);
+        proteinLimit = db.getSetting(proteinKey);
+        if (proteinLimit == -1) {
+            proteinLimit = 50;
+            db.addSetting(proteinKey, proteinLimit);
+        }
+        settingProt.setText(Integer.toString(proteinLimit));
+
+        EditText settingCarb = (EditText) findViewById(R.id.carb_limit);
+        carbLimit = db.getSetting(carbKey);
+        if (carbLimit == -1) {
+            carbLimit = 50;
+            db.addSetting(carbKey, carbLimit);
+        }
+        settingCarb.setText(Integer.toString(carbLimit));
+
+        EditText settingFat = (EditText) findViewById(R.id.fat_limit);
+        fatLimit = db.getSetting(fatKey);
+        if (fatLimit == -1) {
+            fatLimit = 50;
+            db.addSetting(fatKey, fatLimit);
+        }
+        settingFat.setText(Integer.toString(fatLimit));
+
+        EditText settingSodium = (EditText) findViewById(R.id.sodium_limit);
+        sodiumLimit = db.getSetting(sodiumKey);
+        if (sodiumLimit == -1) {
+            sodiumLimit = 50;
+            db.addSetting(sodiumKey, sodiumLimit);
+        }
+        settingSodium.setText(Integer.toString(sodiumLimit));
     }
 
     public void saveSettings(View v) {
@@ -36,6 +76,26 @@ public class SettingsActivity extends CalorieTrackerActivity {
         setting.setText(Integer.toString(caloricLimit));
         db.updateSettings(calorieKey, caloricLimit);
 
+        EditText settingProt = (EditText) findViewById(R.id.protein_limit);
+        if (settingProt.length() != 0) proteinLimit = Integer.parseInt(settingProt.getText().toString());
+        settingProt.setText(Integer.toString(proteinLimit));
+        db.updateSettings(proteinKey, proteinLimit);
+
+        EditText settingFat = (EditText) findViewById(R.id.fat_limit);
+        if (settingFat.length() != 0) fatLimit = Integer.parseInt(settingFat.getText().toString());
+        settingFat.setText(Integer.toString(fatLimit));
+        db.updateSettings(fatKey, fatLimit);
+
+        EditText settingSodium = (EditText) findViewById(R.id.sodium_limit);
+        if (settingSodium.length() != 0) sodiumLimit = Integer.parseInt(settingSodium.getText().toString());
+        settingSodium.setText(Integer.toString(sodiumLimit));
+        db.updateSettings(sodiumKey, sodiumLimit);
+
+        EditText settingCarb = (EditText) findViewById(R.id.carb_limit);
+        if (settingCarb.length() != 0) carbLimit = Integer.parseInt(settingCarb.getText().toString());
+        settingCarb.setText(Integer.toString(carbLimit));
+        db.updateSettings(carbKey, carbLimit);
+        
         finish();
 
     }
